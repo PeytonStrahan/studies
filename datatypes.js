01103a8e54d806e384e68e6f4d444c02b364c044
PeytonStrahan <peytonstrahan@outlook.com>
2024-09-29 18:13:47 -0500

    started on data types

--- datatypes.js
+++ datatypes.js
@@ -1,5 +1,10 @@
 /**
- * 0: Data types
+ * 0: Data types are the many different types of data (or values) that can be stored in variables and utilized by some part of the program to help
+ * complete a task (which, depending on the data type, can possibly involve making new data, seeing whether or not a piece of code should run,
+ * containing a certain section of code to be run, or most other actions that a program can take). The different types of data include the number,
+ * string, boolean, array, object, function, undefined, null, NaN, infinity, and negative infinity (-infinity). The many different types of data have
+ * several unique properties that make them useful in certain situations. Numbers let you do mathematical operations, Strings let you store and
+ * display something like a name or message, arrays let you store a list of items, etc.
  * 
  * 1. Number: 
  * 
@@ -17,7 +22,7 @@
  * 
  * 8. null: 
  * 
- * 9 NaN: 
+ * 9. NaN: 
  * 
  * 10. Infinity and -Infinity: 
  * 
